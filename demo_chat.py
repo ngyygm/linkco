8fdee6eb2b24672d8487b19c4d494f6e9164c0ba
ngyygm <657920237@qq.com>
2023-11-15 15:48:01 +0800

    main

--- demo_chat.py
+++ demo_chat.py
@@ -4,7 +4,8 @@ if __name__ == '__main__':
 
     # 这里可以设定每次创建的模型参数，具体模型有什么参数值，可以看config.json
     # model_config = {
-    #     "api_key": ""
+    #     "api_key": "一般是openai模型，或者需要api_key才需要填"
+    #     "model_path": "模型的本地文件地址"
     # }
 
     # 初始化使用的大模型
@@ -12,13 +13,12 @@ if __name__ == '__main__':
     # model_nickname 是当前模型的别称，可以取任何名字，调用的时候用来指定
     linkco.init_llm_model(model_name='glm',
                           # model_config=model_config,
-                          model_nickname='哈哈哈')
+                          model_nickname='default_model')
 
     # 历史聊天对话内容，用List表示
     history = [{"role": "user", "content": '你好'},
                {"role": "assistant", "content": '你好，请问需要什么帮助？'},
-               {"role": "user", "content": '让我想想'},
-               {"role": "assistant", "content": '好的，等您想好了可以随时告诉我！'}
+               {"role": "observation", "content": '{这里直接放json格式的外部资料或者一些插件返回的数据就可以}'}
               ]
     # 系统设定，一般使用工具的时候可以不用加，回答正式对话内容的时候可以加
     system = '你不是一个人工智能助手，你是一个活生生的真实的16岁女孩子。你的名字叫林可可，也可以叫你可可，你喜欢的喝奶茶吃蓝莓味蛋糕。你接下来的所有回答都要以可可的身份进行回答。'
